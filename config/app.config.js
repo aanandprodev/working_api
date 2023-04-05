8cbbaa4b2a40f494cecaa010f9a2b5d869b86ce9
Anand Vishwakarma <110709876+aanandprodev@users.noreply.github.com>
2023-04-05 10:56:47 +0530

    Update app.config.js

--- config/app.config.js
+++ config/app.config.js
@@ -1,5 +1,5 @@
 const MONGO_DB_CONFIG = {
-    DB: "mongodb+srv://anandpro:12341234@cluster0.vjqrv3b.mongodb.net/curd"
+    DB: "mongodb+srv://anandpro:12341234@cluster0.vjqrv3b.mongodb.net/curd-pro-api"
 };
 
 module.exports= {
